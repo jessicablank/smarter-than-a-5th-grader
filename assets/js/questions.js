cfb3586570efadafab7e08006cd0e1bc70db415c
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-12-13 21:02:47 -0800

    added questions file

---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+let questions = [
+   { title: 'Who was the last Queen of France?',
+    options: ['Marie Antoinette', 'Carrie Fisher', 'Marie Curie'],
+    correctAnswer: 'Marie Antoinette'
+},
+{ title: 'What empire was Julius Caesar was the emperor of?',
+options: ['Greek', 'Roman', 'Chinese','Ottoman' ],
+correctAnswer: 'Roman'
+},
+{ title: 'What is the mountain range that includes Mount Everest called?',
+options: ['The Rockies', 'The Alps', 'The Himalayas','The Pyrenees' ],
+correctAnswer: 'The Himalayas'
+},
+{ title: 'The Earth has four layers. We live on the crust, which is the thinnest layer. ',
+options: ['true', 'false',],
+correctAnswer: 'false'
+}
+]
